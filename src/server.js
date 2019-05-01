1cad7ff82bbce9276e834a5dd3139beca2ff7b38
Gift <laurandidi21@gmailcom>
2019-05-01 18:29:08 +0100

    Add files for lesson 1

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -11,5 +12,18 @@ app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
+app.use('/api', router)
 
-export const start = () => {}
+//  order for routes matters a lot arrange it from top to bottom
+router.get('/data', (req, res) => {
+  res.send({ message: 1 })
+})
+router.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is running at 3000')
+  })
+}
