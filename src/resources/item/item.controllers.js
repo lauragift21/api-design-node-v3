fc5865735018d5fb832c5d61cfada1cde8714ad4
Gift <laurandidi21@gmailcom>
2019-05-01 22:16:54 +0100

    add models

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,15 @@
-export default {}
+import { Item } from './item.model'
+import mongoose from 'mongoose'
+import { connect } from '../../utils/db'
+
+const run = async () => {
+  await connect('mongodb://localhost:27017/api-test')
+  const item = await Item.create({
+    name: 'Clean up',
+    createdBy: mongoose.Types.ObjectId(),
+    list: mongoose.Types.ObjectId()
+  })
+  console.log(item)
+}
+
+run()
